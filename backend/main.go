f719a533afdd83891bf2b8c3371a28202f049c19
Nico Renaldo <nicorenaldo14@gmail.com>
2021-10-02 10:34:50 +0700

    add and get product

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/radityaqb/tgtc/backend/handlers"
 	"github.com/radityaqb/tgtc/backend/server"
 )
+
+type Product struct {
+	ID           int64   `json:"id"`
+	Name         string  `json:"name"`
+	ShopName     string  `json:"shop_name"`
+	ProductPrice float64 `json:"product_price"`
+	ImageURL     string  `json:"image_url"`
+}
+
+type APIResponse struct {
+	Data  interface{} `json:"data"`
+	Error string      `json:"error_message"`
+}
+
+var products = []Product{
+	Product{
+		ID:           1,
+		Name:         "Nama1",
+		ShopName:     "Tokopedia",
+		ProductPrice: 666.0,
+		ImageURL:     "www.google.com",
+	},
+}
+var APIResponses = []APIResponse{}
 
 func main() {
 
@@ -22,7 +48,9 @@ func main() {
 
 	// construct your own API endpoints
 	// endpoint : /add-product
+	router.HandleFunc("/add-product", addProduct).Methods(http.MethodPost)
 	// endpoint : /get-product?id=
+	router.HandleFunc("/get-product/{id}", getProduct).Methods(http.MethodGet)
 	// endpoint : /update-product
 	// endpoint : /delete-product
 
@@ -32,4 +60,26 @@ func main() {
 		Port:         8000,
 	}
 	server.Serve(serverConfig, router)
+}
+
+func getProduct(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	for _, product := range products {
+		dump, _ := strconv.ParseInt(params["id"], 10, 64)
+		if product.ID == dump {
+			json.NewEncoder(w).Encode(product)
+			return
+		}
+	}
+}
+
+func addProduct(w http.ResponseWriter, r *http.Request) {
+	product := Product{}
+	_ = json.NewDecoder(r.Body).Decode(&product)
+	// product.ID = strconv.Itoa(len(products) + 1)
+	product.ID = int64(len(products) + 1)
+	products = append(products, product)
+
+	json.NewEncoder(w).Encode(product)
 }
